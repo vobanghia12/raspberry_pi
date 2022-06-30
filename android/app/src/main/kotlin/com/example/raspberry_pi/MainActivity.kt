a4551daa391229bc490d0a0e1eb4edfe7350a233
Nghia Vo <vobanghia12@gmail.com>
2022-06-30 18:34:04 -0400

    check

---
+++ android/app/src/main/kotlin/com/example/raspberry_pi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.raspberry_pi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_vlc_player/flutter_vlc_player.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final VlcPlayerController _videoPlayerController = VlcPlayerController.network(
+      'http://192.168.254.14:8081/video',
+      hwAcc: HwAcc.full,
+      autoPlay: true,
+      options: VlcPlayerOptions(),
+    );
+
+  Future<void> initializePlayer() async {}
+
+  @override
+  
+
+  @override
+  void dispose() async {
+    super.dispose();
+    await _videoPlayerController.stopRendererScanning();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: VlcPlayer(
+            controller: _videoPlayerController,
+            aspectRatio: 9/16,
+            placeholder: const Center(child: CircularProgressIndicator()),
+          ),
+        ));
+  }
+}
